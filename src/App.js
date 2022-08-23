512ab79e45eb2ce397041cea9668fcde67174395
Pabz Veroy <pabzveroy@Pabzs-MacBook-Pro.local>
2022-08-23 22:20:13 +0800

    scale app height

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import './App.css';
 import Stage1 from 'components/Stage1';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [windowHeight, setWindowHeight] = useState(window.innerHeight);
+
+  useEffect(() => {
+    window.onresize = function () {
+      setWindowHeight(window.innerHeight)
+    }
+  }, [])
 
   return (
-    <div className='App'>
-
-        <Stage1 />
+    <div className='app' style={{ height: windowHeight }}>
+      <Stage1 />
     </div>
   );
 }
+
+
 
 export default App;
