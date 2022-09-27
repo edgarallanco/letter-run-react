d481641f078526f907137718075d90659cf921bb
WItt Irion Langstaff, III <witt@edgarallan.com>
2022-09-27 10:47:53 -0400

    flying adjust

--- src/components/scripts/IntroAnimation.js
+++ src/components/scripts/IntroAnimation.js
@@ -3,14 +3,14 @@ import { Vector3 } from 'three';
 
 export const animate = (camera, cameraMesh, setCamPosition, initialPos) => {
   return new Promise((resolve, reject) => {
-    let animDuration = 2.5;
+    let animDuration = 5;
     // console.log(cameraMesh.position);
     // let initialPosition = new Vector3();
     let camInitialPos = new Vector3(initialPos[0], initialPos[1], initialPos[2]);
     camera.position.copy(camInitialPos);
     // console.log(camera.position);
     gsap.to(camInitialPos, {
-      ease: "none", duration: (animDuration - .5), delay: .5, x: 5.78651222602025, y: 72.32470228479104, z: 60.826936793399625,
+      ease: "easeInOutCubic", duration: (animDuration), delay: 0, x: 5.78651222602025, y: 72.32470228479104, z: 60.826936793399625,
       onUpdate: () => {
         setCamPosition(camInitialPos);
       },
@@ -19,8 +19,8 @@ export const animate = (camera, cameraMesh, setCamPosition, initialPos) => {
       }
     });
 
-    gsap.to(cameraMesh.position, { ease: "none", duration: (animDuration - 1), delay: 1, x: -57.53, z: -8 })
-    gsap.to(camera, { ease: "none", duration: animDuration, zoom: 4.5 })
+    gsap.to(cameraMesh.position, { ease: "easeInOutCubic", duration: (animDuration - 1), delay: 1, x: -57.53, z: -8 })
+    gsap.to(camera, { ease: "easeInOutCubic", duration: animDuration, zoom: 6.5 })
   });
 
 }
