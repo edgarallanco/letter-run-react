189ab06955796e7a6a969473f333ab7722a5712a
Pabz Veroy <pabzveroy@Pabzs-MacBook-Pro.local>
2022-09-28 22:42:24 +0800

    update on intro animation

--- src/components/scripts/IntroAnimation.js
+++ src/components/scripts/IntroAnimation.js
@@ -3,21 +3,22 @@ import { Vector3, Euler } from 'three';
 
 export const animate = (camera, cameraMesh, setCamPosition, initialPos) => {
   return new Promise((resolve, reject) => {
-    let animDuration = 2.5;
+    let animDuration = 4;
     // console.log(cameraMesh.position);
     // let initialPosition = new Vector3();
     let camInitialPos = new Vector3(initialPos[0], initialPos[1], initialPos[2]);
     camera.position.copy(camInitialPos);
     // console.log(camera.position);
     gsap.to(camInitialPos, {
-      ease: "none", duration: (animDuration - .5), delay: .5, x: 5.78651222602025, y: 72.32470228479104, z: 60.826936793399625,
+      ease: "none", duration: animDuration, x: 5.78651222602025, y: 72.32470228479104, z: 60.826936793399625,
       onComplete: () => {
         resolve()
       }
     });
 
-    gsap.to(cameraMesh.position, { ease: "none", duration: (animDuration - 1), delay: 1, x: -57.53, z: -8, 
+    gsap.to(cameraMesh.position, { ease: "none", duration: (animDuration - 1.5), delay: 1.5, x: -57.53, z: -8, 
       onUpdate: () => {
+        console.log(camera.rotation);
         setCamPosition(camInitialPos);
       } })
     gsap.to(camera, { ease: "none", duration: animDuration, zoom: 4.5 })
