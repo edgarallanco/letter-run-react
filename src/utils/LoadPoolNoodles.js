764e6080a2974aba789fba37ed0c9c377eaeccb5
Pabz Veroy <pabzveroy@Pabzs-MacBook-Pro.local>
2022-09-30 18:18:34 +0800

    update files

--- src/utils/LoadPoolNoodles.js
+++ src/utils/LoadPoolNoodles.js
@@ -12,7 +12,7 @@ export const noodles = [
   // },
   {
     name: "ring_1",
-    filename: "./../resources/models/pink_ring.glb",
+    filename: "https://fargamot.s3.amazonaws.com/resources/models/pink_ring.glb",
     position: new Vector3(35.51932849514347, 3.0149306909942626, 15.341705365864758),
     // geometry: new SphereGeometry(1, 20, 10),
     geometry: new CylinderGeometry(1, 1, 0.5, 100),
@@ -22,7 +22,7 @@ export const noodles = [
   },
   {
     name: "ring_2",
-    filename: "./../resources/models/yellow_ring.glb",
+    filename: "https://fargamot.s3.amazonaws.com/resources/models/yellow_ring.glb",
     position: new Vector3(43.70883094989104, 3.0149306909942626, 18.540877822233906),
     // geometry: new TorusGeometry(0.8, 0.2, 20, 100),
     // geometry: new SphereGeometry(1, 20, 10),
@@ -33,7 +33,7 @@ export const noodles = [
   },
   {
     name: "noodle",
-    filename: "./../resources/models/purple_noodle.glb",
+    filename: "https://fargamot.s3.amazonaws.com/resources/models/purple_noodle.glb",
     position: new Vector3(33.5110924256302, 3.0149306909942626, 27.12094054470053),
     geometry: new CylinderGeometry(1, 1, 0.5, 100),
     rotation: new Euler(0, 0, 0),
@@ -42,7 +42,7 @@ export const noodles = [
   },
   {
     name: "ball",
-    filename: "./../resources/models/white_ball.glb",
+    filename: "https://fargamot.s3.amazonaws.com/resources/models/white_ball.glb",
     position: new Vector3(38.72937366844723, 3.0149306909942626, 21.85546584699992),
     geometry: new CylinderGeometry(0.6, 0.6, 0.5, 100),
     rotation: new Euler(0, 0, 0),
